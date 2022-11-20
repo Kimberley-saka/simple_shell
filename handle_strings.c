22192ac5b1bc03de28dfa537ebb28dfa496b33c0
kimberley-saka <kimberleysaka@gmail.com>
2022-11-20 05:28:00 -0800

    functions to handle strings, such as copying strings

--- handle_strings.c
+++ handle_strings.c
@@ -1,34 +1,76 @@
 #include "shell.h"
 
 /**
- * _strlen - find the length of a string
- * @s: pointer to the string to check
- * Return: void
+ * *_strncpy - copies a string
+ * @dest: the destination string to be copied to
+ * @src: the source string
+ * @n: the amount of characters to be copied
+ * Return: the concatenated string
  */
 
-int _strlen(const char *s)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i, j;
+	char *s = dest;
 
-	while (s[i])
-		i++;
-	return (i);
-}
-/**
- * _strcpy -  copies the string pointed to by src
- * @dest: char type string
- * @dest: char type string
- * @src: char type string
- * Return: Pointer to `dest`
- */
-
-char *_strcpy(char *dest, const char *src)
-{
-	int i = -1;
-
-	do {
-		i++;
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
 		dest[i] = src[i];
-	} while (src[i] != '\0');
-	return (dest);
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ * *_strncat - concatenates two strings
+ * @dest: the first string
+ * @src: the second string
+ * @n: the amount of bytes to be maximally used
+ * Return: the concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: the string to be parsed
+ * @c: the character to look for
+ * Return: (s) a pointer to the memory area
+ */
+
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+	return (NULL);
 }
